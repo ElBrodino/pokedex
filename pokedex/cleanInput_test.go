021ba62419c7a794e55317f16bb4d408cd951200
Frederic Poirier <uk_jobs@hotmail.com>
2026-02-08 13:22:05 -0500

    tests working for spaces

--- pokedex/cleanInput_test.go
+++ pokedex/cleanInput_test.go
@@ -1,15 +1,42 @@
 package main
 
 import (
-	"reflect"
 	"testing"
 )
 
 func TestCleanInput(t *testing.T) {
-	got := cleanInput("	hello	world	")
-	want := []string{"hello", "world"}
-	if !reflect.DeepEqual(want, got) {
-		t.Fatalf("expected: %v, got: %v", want, got)
+	cases := []struct {
+		name     string
+		input    string
+		expected []string
+	}{
+		{
+			name:     "spaces front and back",
+			input:    "	hello	world	",
+			expected: []string{"hello", "world"},
+		}, /*
+			{
+				name:     "mixed case words",
+				input:    "Charmander Balbasaur PIKACHU",
+				expected: []string{"charmander", "balbasaur", "pikachu"},
+			},*/
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := cleanInput(c.input)
+
+			if len(got) != len(c.expected) {
+				t.Fatalf("len mismatch: got %v, expected %v", got, c.expected)
+			}
+
+			for i := range got {
+				if got[i] != c.expected[i] {
+					t.Fatalf("cleanInput(%q)[%d] = %q, expected %q",
+						c.input, i, got[i], c.expected[i])
+				}
+			}
+		})
 	}
 }
 
