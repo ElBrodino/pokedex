7af058e4ded2a19f9add229d5dc9d036f6543904
Frederic Poirier <uk_jobs@hotmail.com>
2026-02-08 13:28:46 -0500

    moved to REPL way of thinking (Read, Eval, Print, Loop)

--- pokedex/repl.go
+++ pokedex/repl.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"strings"
+)
+
+func cleanInput(text string) []string {
+	lowered := strings.ToLower(text)
+	answer := strings.Join(strings.Fields(lowered), " ")
+	return strings.Split(answer, " ")
+}
