6f12bc933c030ecabbbdffff76d4acdb9e41a65c
Frederic Poirier <uk_jobs@hotmail.com>
2026-02-08 17:39:49 -0500

    shorter unit tests with any type of expected result

--- pokedex/repl_test.go
+++ pokedex/repl_test.go
@@ -5,34 +5,25 @@ import (
 	"testing"
 )
 
-func assertEqual(t *testing.T, got, want any) {
+func assertEqual(t *testing.T, desc, got, want any) {
 	t.Helper()
 
 	if !reflect.DeepEqual(got, want) {
-		t.Fatalf("Expected %#v, got %#v", want, got)
+		t.Fatalf("\n*** %#v ***\n Expected %#v, got %#v", desc, want, got)
 	}
 }
 
-func TestCleanInput(t *testing.T) {
-	cases := []struct {
-		//name     string
-		input    string
-		expected []string
-	}{
-		{
-			//name:     "spaces front and back",
-			input:    "	hello	world	",
-			expected: []string{"hello", "world"},
-		},
-		{
-			//name:     "mixed case words",
-			input:    "Charmander Balbasaur PIKACHU",
-			expected: []string{"charmander", "balbasaur", "pikachu"},
-		},
-	}
-
-	for _, c := range cases {
-		actual := cleanInput(c.input)
-		assertEqual(t, actual, c.expected)
-	}
+func TestStuff(t *testing.T) {
+	assertEqual(t, "Spaces front and back and middle",
+		cleanInput("   	hello	world   "),
+		[]string{"hello", "world"})
+	assertEqual(t, "Spaces front and back and middle, amount",
+		len(cleanInput("   	hello	world   ")),
+		2)
+	assertEqual(t, "mixed case words",
+		cleanInput("Charmander Balbasaur PIKACHU"),
+		[]string{"charmander", "balbasaur", "pikachu"})
+	assertEqual(t, "empty input",
+		cleanInput(""),
+		[]string{""})
 }
