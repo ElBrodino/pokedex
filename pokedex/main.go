9c94bdd13eab19ffa7e36d956f56f9fcf964376f
Frederic Poirier <uk_jobs@hotmail.com>
2026-02-09 11:00:04 -0500

    refactor to simplify the main

--- pokedex/main.go
+++ pokedex/main.go
@@ -1,63 +1,6 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
 	//fmt.Println("Hello, World!")
-
-	var commands = map[string]cliCommand{
-		"exit": {
-			name:        "exit",
-			description: "Exit the Pokedex",
-			callback:    commandExit,
-		},
-		"help": {
-			name:        "help",
-			description: "Displays a help message",
-			callback:    commandHelp,
-		},
-	}
-
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		text := cleanInput(scanner.Text())
-
-		cmd, ok := commands[text[0]]
-		if len(text) == 0 {
-			continue
-		}
-
-		if !ok {
-			fmt.Println("Unknown command")
-			continue
-		}
-		if err := cmd.callback(); err != nil {
-			fmt.Println("Error:", err)
-		}
-		//fmt.Println("Your command was:", text[0])
-
-	}
-}
-
-func commandExit() error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
-}
-
-func commandHelp() error {
-	fmt.Println("Welcome to the Pokedex!\nUsage:\n\nhelp: Displays a help message\nexit: Exit the Pokedex")
-	return nil
-}
-
-type cliCommand struct {
-	name        string
-	description string
-	callback    func() error
+	startRepl()
 }
--- pokedex/repl.go
+++ pokedex/repl.go
@@ -1,11 +1,57 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func startRepl() {
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		text := cleanInput(scanner.Text())
+
+		cmd, ok := commands[text[0]]
+		if len(text) == 0 {
+			continue
+		}
+
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+		if err := cmd.callback(); err != nil {
+			fmt.Println("Error:", err)
+		}
+		//fmt.Println("Your command was:", text[0])
+
+	}
+}
 func cleanInput(text string) []string {
 	lowered := strings.ToLower(text)
 	answer := strings.Join(strings.Fields(lowered), " ")
 	return strings.Split(answer, " ")
+}
+
+var commands = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+	"help": {
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp,
+	},
 }
