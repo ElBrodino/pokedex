b6fa7ec159d4fac7eec4f02628db0b5ed669f91e
Frederic Poirier <uk_jobs@hotmail.com>
2026-02-09 09:58:47 -0500

    integrate into the terminal

--- pokedex/main.go
+++ pokedex/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	//fmt.Println("Hello, World!")
+	for {
+		scanner := bufio.NewScanner(os.Stdin)
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		text := cleanInput(scanner.Text())
+
+		fmt.Println("Your command was:", text[0])
+
+	}
 }
