ba4bab6a0b208c2d08bab5332da7beeff511a083
Frédéric Poirier <uk_jobs@hotmail.com>
2026-02-11 11:24:34 -0500

    added map & mapbfunctionality

--- pokedex/command_map.go
+++ pokedex/command_map.go
@@ -48,5 +48,34 @@ func commandMap(cfg *config) error {
 }
 
 func commandMapB(cfg *config) error {
+	url := "https://pokeapi.co/api/v2/location-area/"
+	if cfg.prevLocationsURL == nil {
+		fmt.Println("you're on the first page")
+		return nil
+	} else {
+		url = *cfg.prevLocationsURL
+	}
+	// request
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	// decode JSON
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("pokeapi returned status %s", resp.Status)
+	}
+
+	var data locationAreaResponse
+	err2 := json.NewDecoder(resp.Body).Decode(&data)
+	if err2 != nil {
+		return err2
+	}
+	for _, r := range data.Results {
+		fmt.Println(r.Name)
+	}
+	cfg.nextLocationsURL = data.Next
+	cfg.prevLocationsURL = data.Previous
+
 	return nil
 }
